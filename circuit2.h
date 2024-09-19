af0a79ea91e0b537295639f196775427c7ac63af
Bartek Osiak <osiakbartek5@gmail.com>
2024-09-19 09:36:41 +0200

    ...

---
+++ circuit2.h
@@ -0,0 +1,21 @@
+#include "Elements.h"
+#include "Signals.h"
+#include <iostream>
+#include <vector>
+#include <complex>
+
+using namespace std;
+
+class Circuit{
+
+    private:
+
+    vector<Element> Componets;
+
+    public:
+
+    Circuit(vector<Element> componets) : Componets(componets){}
+
+
+
+};
