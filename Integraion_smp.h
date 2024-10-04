aff680d5a6617fa24f26cd7a323b274c28e2a505
unknown <osiakbartek5@gmail.com>
2024-10-04 18:37:07 +0200

    ...

--- Integraion_smp.h
+++ Integraion_smp.h
@@ -7,20 +7,19 @@ using namespace std;
 
 double integrate(function<double(double)>f, double x1, double x2, const int n = 1000000){
 
-    double sum = 0;
     double result = 0;
-    double x = 0;
+    double sum = 0;
     double dx = (x2 - x1) / n;
 
     for(int i = 1; i < n; i++){
-        x = x1 + (i * dx);
-        sum += f((x - dx) / 2);
-        result += f(x);
+        sum += f((x1 + (i * dx)) - dx/2);
+        result += f(x1 + i * dx);
     }
-    sum += f((x2 - dx) / 2);
-    result = (dx / 6) * f(x1) + f(x2) + (2 * result) + (4 * sum);
 
-    return result; 
+    sum += f(x2 - dx/2);
+    result = (dx/6) * (f(x1) + f(x2) + 2 * result + 4 * sum);
+
+    return result;
 }
 
 #endif
